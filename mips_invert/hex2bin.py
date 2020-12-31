7d3d77be837aec19c49f7597a2536d56edd94532
innns <ureinsecure@outlook.com>
2020-12-31 17:18:27 +0800

    update

--- mips_invert/hex2bin.py
+++ mips_invert/hex2bin.py
@@ -5,20 +5,11 @@
 # Email: ureinsecure@outlook.com
 # Date: 2020-12-31 17:14:16
 # LastEditors: Zx
-# LastEditTime: 2020-12-31 17:14:16
+# LastEditTime: 2020-12-31 17:18:21
 # FilePath: /MIPS/mips_invert/hex2bin.py
 # <<<<<<
-# >>>>>>
-# Descripttion: 
-# version: 1.0
-# Author: Zx
-# Email: ureinsecure@outlook.com
-# Date: 2020-12-31 16:50:58
-# LastEditors: Zx
-# LastEditTime: 2020-12-31 17:03:49
-# FilePath: /MIPS/mips_invert/16to2.py
-# <<<<<<
-f = open("mips_invert/binary.txt","r+")
+
+f = open("mips_invert/imem.txt","r+")
 a = ""
 try:
     while True:
@@ -31,7 +22,6 @@ try:
 finally:
     f.close()
 
-f = open("mips_invert/binary","w+")
+f = open("mips_invert/binary.txt","w")
 f.write(a)
 f.close()
-
